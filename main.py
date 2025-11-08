59a9749900c14af46ef628198395c0618f6511a6
.c6 <70641065+KraSHrspam@users.noreply.github.com>
2025-11-08 12:28:25 +0300

    Update main.py + req-ts + .gitignore

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import collections
 import pandas
+import argparse
 
 env = Environment(
     loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
@@ -25,7 +26,18 @@ def calculate_company_age():
     return "лет"
 
 
-wine_raw_excel_data = pandas.read_excel("wine.xlsx", keep_default_na=False)
+parser = argparse.ArgumentParser(
+    description='Запускает сайт с сортами вин. Автоматически заполняет название, цену, картинку, скидку и сорт винограда (Если он есть) напитка по таблице Excel.'
+)
+parser.add_argument(
+    '-w', '--wines',
+    default='wine.xlsx',
+    help='Путь до таблицы с винами.'
+)
+args = parser.parse_args()
+
+
+wine_raw_excel_data = pandas.read_excel(args.wines, keep_default_na=False)
 wines_excel_data = wine_raw_excel_data.to_dict(orient="records")
 template = env.get_template("template.html")
 wines = collections.defaultdict(list)
@@ -34,7 +46,9 @@ for wine in wines_excel_data:
     wines[wine["Категория"]].append(wine)
 
 rendered_page = template.render(
-    age=company_age, years=calculate_company_age(), wines=wines
+    age=company_age,
+    years=calculate_company_age(),
+    wines=wines
 )
 
 with open("index.html", "w", encoding="utf8") as file:
