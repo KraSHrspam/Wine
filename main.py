938bc3ceb6f6816bc03bdf64c28fd5a5a15dba7a
.c6 <70641065+KraSHrspam@users.noreply.github.com>
2025-11-11 17:58:46 +0300

    Summary(ifmain + req)

--- main.py
+++ main.py
@@ -5,54 +5,52 @@ import collections
 import pandas
 import argparse
 
-env = Environment(
-    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
-)
-
-today_date = datetime.datetime.now().year
-foundation_date = 1920
-company_age = today_date - foundation_date
-
-
 def calculate_company_age():
-    delta_new = company_age % 100
-    if 21 > delta_new > 4:
+        delta_new = company_age % 100
+        if 21 > delta_new > 4:
+            return "лет"
+        delta_new = company_age % 10
+        if delta_new == 1:
+            return "год"
+        elif 1 < delta_new < 5:
+            return "года"
         return "лет"
-    delta_new = company_age % 10
-    if delta_new == 1:
-        return "год"
-    elif 1 < delta_new < 5:
-        return "года"
-    return "лет"
 
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+    )
 
-parser = argparse.ArgumentParser(
-    description='Запускает сайт с сортами вин. Автоматически заполняет название, цену, картинку, скидку и сорт винограда (Если он есть) напитка по таблице Excel.'
-)
-parser.add_argument(
-    '-w', '--wines',
-    default='wine.xlsx',
-    help='Путь до таблицы с винами.'
-)
-args = parser.parse_args()
+    today_date = datetime.datetime.now().year
+    foundation_date = 1920
+    company_age = today_date - foundation_date
 
+    parser = argparse.ArgumentParser(
+        description='Запускает сайт с сортами вин. Автоматически заполняет название, цену, картинку, скидку и сорт винограда (Если он есть) напитка по таблице Excel.'
+    )
+    parser.add_argument(
+        '-w', '--wines',
+        default='wine.xlsx',
+        help='Путь до таблицы с винами.'
+    )
+    args = parser.parse_args()
 
-wine_raw_excel_data = pandas.read_excel(args.wines, keep_default_na=False)
-wines_excel_data = wine_raw_excel_data.to_dict(orient="records")
-template = env.get_template("template.html")
-wines = collections.defaultdict(list)
+    wine_raw_excel_data = pandas.read_excel(args.wines, keep_default_na=False)
+    wines_excel_data = wine_raw_excel_data.to_dict(orient="records")
+    template = env.get_template("template.html")
+    wines = collections.defaultdict(list)
 
-for wine in wines_excel_data:
-    wines[wine["Категория"]].append(wine)
+    for wine in wines_excel_data:
+        wines[wine["Категория"]].append(wine)
 
-rendered_page = template.render(
-    age=company_age,
-    years=calculate_company_age(),
-    wines=wines
-)
+    rendered_page = template.render(
+        age=company_age,
+        years=calculate_company_age(),
+        wines=wines
+    )
 
-with open("index.html", "w", encoding="utf8") as file:
-    file.write(rendered_page)
+    with open("index.html", "w", encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
